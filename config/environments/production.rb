6e6d0ed832542d3ed99f0031f1f2b1f36a7b4467
Cesar <cesarmota@ymail.com>
2020-08-19 13:46:52 -0500

    edits for online deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  Rails.application.routes.default_url_options = { host: "https://eager-bell-510966.netlify.app" }
+
 end
